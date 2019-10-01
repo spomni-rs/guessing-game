c2dc1df5578951a0ec230a58c7ad41668643a214
spomni <mikhail.snitko@gmail.com>
2019-10-01 09:21:45 +0300

    feat: Find solution.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor(){
+      this._max = null;
+      this._min = null;
+      this._guess = null;
+    }
 
     setRange(min, max) {
-
+      this._min = min;
+      this._max = max;
     }
 
     guess() {
+      let min = this._min;
+      let max = this._max;
 
+      this._guess = min + Math.ceil((max - min) / 2);
+
+      return this._guess;
     }
 
     lower() {
-
+      this._max = this._guess;
     }
 
     greater() {
-
+      this._min = this._guess;
     }
 }
 
